bb34ccaeb91e10e93d5de0d27799c59ec325177f
ayeshaali <aliayeshanyc@gmail.com>
2019-02-09 14:15:21 -0500

    set up

---
+++ mvc_server.js
@@ -0,0 +1,21 @@
+//Networks
+
+var express = require('express');
+var fs = require('fs');
+var favicon = require('serve-favicon');
+
+var app = express();
+app.use(express.static('public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+// app.use(favicon(__dirname + '/public/images/logo.png'));
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Server started at '+ new Date()+', on port ' + port+'!');
+});
+
+//first request, renders index
+app.get('/', function(request, response){
+  response.render('index', {page:request.url, title:"Index"});
+});

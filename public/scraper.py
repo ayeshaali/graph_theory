0dcde1bcdf7fce0b188b8254fc6455affbfb05bc
ayeshaali <aliayeshanyc@gmail.com>
2019-02-20 20:29:43 -0500

    message

--- public/scraper.py
+++ public/scraper.py
@@ -1,4 +1,30 @@
+
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+    
+def hasCommas(inputString):
+    return any(char== "," for char in inputString)
+    
+def hasPeriod(inputString):
+    return any(char== "." for char in inputString)
+
 file = open('Grade 12_2nd Semester.txt','r')
-f = file.read()
-print(f)
+lines = file.readlines()
+array = []
+file1 = open('output.txt','w')
+
+for word in lines: 
+    if word.isupper():
+        array.append(word)
+    if "Sp" in word: 
+        array.append(word)
+    if hasCommas(word): 
+        array.append(word)
+    if hasNumbers(word):
+        if "English" in word: 
+            array.append(word)
+        
+array = [word for word in array if not hasCommas(word) & hasPeriod(word)]
+array = [word for word in array if "R:" not in word]
+file1.write(" ".join(str(x) for x in array))
 file.close()
